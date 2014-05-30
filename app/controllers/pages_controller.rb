443ea58d229350b578ecc34a5fafe1b44ffc7bb8
Bill Fienberg <billfienberg@gmail.com>
2014-05-30 11:12:17 -0500

    Empty index action of pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @origin = Origin.where(zip:95688).first.id
-    @destination = Destination.where(zip:90017).first.id
-    @carriers = CarrierDestination.where(destination_id: 4)
+    
   end
 
   def show

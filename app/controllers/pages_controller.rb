d2c0527ab4672ba292c950d8ca4baf8d4ea78429
Bill Fienberg <billfienberg@gmail.com>
2014-05-30 10:02:27 -0500

    Add pre heroku-push commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @origin = Origin.where(zip:95688).first.id
     @destination = Destination.where(zip:90017).first.id
-    @carriers = CarrierDestination.where(destination_id: 1)
+    @carriers = CarrierDestination.where(destination_id: 4)
   end
 
   def show

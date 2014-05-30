40d63f8c72651d750874802cddf6c208a73fdbd1
Bill Fienberg <billfienberg@gmail.com>
2014-05-30 16:08:39 -0500

    Add working jquery function to display what days of the week the selected carrier is able to deliver

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
   console.log("hello");
+
   $("td a").on("click", function(e){
     e.preventDefault();
-    $(".days").show();
-    console.log($(this).attr('value'));
+    $(".allowed, .days").hide();
+    var name = $(this).attr('value');
+    console.log($(this).parent().parent().nextUntil("tr.location", "tr."+name));
+    $(this).parent().parent().next().show()
+    $(this).parent().parent().nextUntil("tr.location", "tr."+name).show()
   });
+
 });

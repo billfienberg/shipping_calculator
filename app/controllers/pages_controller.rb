bec7eba4fb2de8d840c7a4367c2f08c2571e4766
Bill Fienberg <billfienberg@gmail.com>
2014-05-29 19:02:33 -0500

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
+    @origin = Origin.where(zip:95688).first.id
+    @destination = Destination.where(zip:90017).first.id
+    @carriers = CarrierDestination.where(destination_id: 1)
+  end
+
+  def show
   end
 end

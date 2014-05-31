d932001d8001f58165943b1c5fd425e74e69aa7e
Bill Fienberg <billfienberg@gmail.com>
2014-05-30 21:38:43 -0500

    Remove console.logs from destinations.js

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-  console.log("hello");
-
+  
   $("td a").on("click", function(e){
     e.preventDefault();
     $(".allowed, .days").hide();
     var name = $(this).attr('value');
-    console.log($(this).parent().parent().nextUntil("tr.location", "tr."+name));
     $(this).parent().parent().next().show()
     $(this).parent().parent().nextUntil("tr.location", "tr."+name).show()
   });

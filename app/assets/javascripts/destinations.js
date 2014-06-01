849ad418ccd80b41aca86d12429cc69570084d63
Bill Fienberg <billfienberg@gmail.com>
2014-06-01 11:50:36 -0500

    Refactor destinations.js

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
-  var click = 0;
-  var clicks = [{click:NaN, id:NaN, context:NaN}];
+  var clicks = [{id:NaN, context:NaN}];
 
 
   $("td a").on("click", function(e){
     e.preventDefault();
-    click += 1;
     var id = $(this).parent().prev().prev().prev().prev().prev().prev().html();
     var context = $(this).attr('value');
     var days = $(this).parent().parent().next();
     var location = $(this).parent().parent();
     var carrier = $(this).parent().parent().nextUntil("tr.location", "tr."+context);
-    clicks.push({click:click, id:id, context:context});
+    clicks.push({id:id, context:context});
     var current = clicks[clicks.length-1];
     var previous = clicks[clicks.length-2];
-    var two_clicks_ago = clicks[clicks.length-3];
     console.log(clicks);
     
     // this = <a class="carrier">
@@ -22,19 +19,10 @@ $(document).ready(function(){
     // this.parent().parent() = <tr class="location">
     // var days = this.parent().parent().next() = <tr class="days">
     // var carrier = this.parent().parent().nextUntil() = All <tr> elements with class="name" until it reaches a <tr> element with class="location"
-    console.log(click);
-    if (current.id === previous.id && current.context === previous.context && click % 2 === 0) {
+    if (current.id === previous.id && current.context === previous.context) {
       if (previous.context === current.context) {
         $(".days, .allowed").hide();
         current.context = NaN;
-      } else {
-        days.show();
-        carrier.show();
-      }
-    } else if (current.id === previous.id && current.context === previous.context && click % 2 !== 0) {
-      if (previous.context === current.context) {
-        current.context = NaN;
-        $(".days, .allowed").hide();
       } else {
         days.show();
         carrier.show();
@@ -58,5 +46,4 @@ $(document).ready(function(){
     }
 
   });
-
 });
